07a59603e70d515317dcd760cc69c725805bd31b
Nirmal Sunny <32394735+nirmalsunny@users.noreply.github.com>
2020-09-26 17:37:29 +0100

    Add files via upload

---
+++ website_category.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+
+
+# This import is needed only when you run this file in isolation.
+import sys
+
+
+geckodriver = 'path/to/your/geckodriver'
+
+def sitereview(url): 
+    options = webdriver.FirefoxOptions()
+    options.add_argument('-headless')
+    base_url = "https://sitereview.bluecoat.com/#/lookup-result/"
+    
+    with webdriver.Firefox(executable_path=geckodriver, options=options) as driver:
+        wait = WebDriverWait(driver, 10)
+        driver.get(base_url + url)
+        first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "clickable-category")))
+        category = first_result.get_attribute("textContent")
+        driver.close
+    return category   
+
+def main(url):
+    print(sitereview(url))
+
+# Use the below two lines if website_category.py is being run as a standalone file. If you are running this file as
+# a part of a workflow pipeline, comment out these two lines.
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+         print("Please use the following format for the command - `python website_category.py <url-to-be-checked>`")
+         exit(0)
+    main(sys.argv[1])
