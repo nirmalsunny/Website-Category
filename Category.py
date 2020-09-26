df57769c626947325d488470ec6c911ea07c2f92
Nirmal Sunny <32394735+nirmalsunny@users.noreply.github.com>
2020-09-26 20:05:44 +0100

    Update Category.py

--- Category.py
+++ Category.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
@@ -8,4 +11,10 @@ chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.binary_location = GOOGLE_CHROME_PATH
 
-driver = webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+with webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options) as driver:
+    wait = WebDriverWait(driver, 10)
+    driver.get(base_url + url)
+    first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "clickable-category")))
+    category = first_result.get_attribute("textContent")
+    driver.quit
+print(category)
