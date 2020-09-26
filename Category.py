d23e1f0019ed879928ae1de78153ddaf29501082
Nirmal Sunny <32394735+nirmalsunny@users.noreply.github.com>
2020-09-26 20:38:25 +0100

    Update Category.py

--- Category.py
+++ Category.py
@@ -1,3 +1,6 @@
+from flask import Flask
+from flask import request, Response
+from flask import render_template
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -6,15 +9,27 @@ from selenium.webdriver.support.ui import WebDriverWait
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--disable-gpu')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.binary_location = GOOGLE_CHROME_PATH
+mitphish = Flask(__name__)
 
-with webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options) as driver:
-    wait = WebDriverWait(driver, 10)
-    driver.get(base_url + url)
-    first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "clickable-category")))
-    category = first_result.get_attribute("textContent")
-    driver.quit
-print(category)
+@mitphish.route('/')
+def homepage():
+    return "MitPhish" #+ path Yay, the file exists! 
+
+
+mitphish.route('/test/<path:path>')
+def show_index(path):
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.binary_location = GOOGLE_CHROME_PATH
+
+    with webdriver.Chrome(execution_path=CHROMEDRIVER_PATH, chrome_options=chrome_options) as driver:
+        wait = WebDriverWait(driver, 10)
+        driver.get(base_url + path)
+        first_result = wait.until(presence_of_element_located((By.CLASS_NAME, "clickable-category")))
+        category = first_result.get_attribute("textContent")
+        driver.quit
+   return category
+
+if __name__ == "__main__":
+	mitphish.run(debug=True, threaded=True)
